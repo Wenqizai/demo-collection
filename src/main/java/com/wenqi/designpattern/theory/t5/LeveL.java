769039c7fc17fdd5ecd4d66bbf91f0a76b731188
liangwenqi <wenqizai@qq.com>
2022-10-05 22:33:01 +0800

    dp-theory05

---
+++ src/main/java/com/wenqi/designpattern/theory/t5/LeveL.java
@@ -0,0 +1,13 @@
+package com.wenqi.designpattern.theory.t5;
+
+/**
+ * @author Wenqi Liang
+ * @date 2022/10/5
+ */
+public class LeveL {
+    public int iniValue;
+
+    public int iniValue() {
+        return iniValue;
+    }
+}
---
+++ src/main/java/com/wenqi/designpattern/theory/t5/MessageQueueClient.java
@@ -0,0 +1,11 @@
+package com.wenqi.designpattern.theory.t5;
+
+/**
+ * @author Wenqi Liang
+ * @date 2022/10/5
+ */
+public class MessageQueueClient {
+    public void send(String message) {
+        System.out.println(message);
+    }
+}

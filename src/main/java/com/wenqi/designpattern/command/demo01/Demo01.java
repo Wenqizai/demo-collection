520ac12c3b21dd89abed3925a169a75f0def9a6d
liangwenqi <liangwenqi@qdama.cn>
2023-08-31 10:27:03 +0800

    设计模式-命令-demo01

---
+++ src/main/java/com/wenqi/designpattern/command/demo01/Demo01.java
@@ -0,0 +1,12 @@
+package com.wenqi.designpattern.command.demo01;
+
+/**
+ * @author liangwenqi
+ * @date 2023/8/30
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
---
+++ src/main/java/com/wenqi/designpattern/command/demo01/PasteCommand.java
@@ -0,0 +1,21 @@
+package com.wenqi.designpattern.command.demo01;
+
+/**
+ * @author liangwenqi
+ * @date 2023/8/30
+ */
+public class PasteCommand extends Command {
+
+    public PasteCommand(Editor editor) {
+        super(editor);
+    }
+
+    @Override
+    public boolean execute() {
+        if (editor.clipboard == null || editor.clipboard.isEmpty()) return false;
+
+        backup();
+        editor.textField.insert(editor.clipboard, editor.textField.getCaretPosition());
+        return true;
+    }
+}

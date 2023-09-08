5c459a8d68b5cf2ac5b3d31bfba891ab3f7f1fa6
liangwenqi <liangwenqi@qdama.cn>
2023-09-08 15:17:32 +0800

    demo

--- src/main/java/com/wenqi/springboot/HelloWorldController.java
+++ src/main/java/com/wenqi/springboot/HelloWorldController.java
@@ -1,9 +1,13 @@
 package com.wenqi.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author liangwenqi
@@ -20,7 +24,24 @@ public class HelloWorldController {
 
     @GetMapping("/world2")
     public String helloWorld2(@RequestParam("id") Long id) {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         System.out.println(id);
         return "HelloWorld2";
+    }
+
+    @PostMapping("/post/world")
+    public String postHelloWorld(@RequestBody String id, HttpServletResponse response) {
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(id);
+        response.setStatus(HttpServletResponse.SC_SERVICE_UNAVAILABLE);
+        return "postHelloWorld";
     }
 }

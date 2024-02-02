106755d04d43761c51e10feef03c792b4416600f
liangwenqi <liangwenqi@qdama.cn>
2024-02-02 14:56:02 +0800

    demo-BinaryTest

---
+++ src/main/java/com/wenqi/example/primitive/BinaryTest.java
@@ -0,0 +1,18 @@
+package com.wenqi.example.primitive;
+
+/**
+ * @author liangwenqi
+ * @date 2024/1/16
+ */
+public class BinaryTest {
+    public static void main(String[] args) {
+        System.out.println(1 & 1);
+        System.out.println(1 & 2);
+        System.out.println(1 & 4);
+        System.out.println(1 & 8);
+
+        System.out.println(1 & 3);
+        System.out.println(1 & 5);
+        System.out.println(1 & 9);
+    }
+}

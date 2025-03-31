be3411f506ce5bd5b1b6f2dd228a58792e86745f
liangwenqi <wenqizai@gmail.com>
2025-03-31 10:38:25 +0800

    es-getById

--- src/main/java/com/wenqi/springboot/elasticsearch/controller/WebsiteController.java
+++ src/main/java/com/wenqi/springboot/elasticsearch/controller/WebsiteController.java
@@ -6,6 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.wenqi.springboot.elasticsearch.model.ResponseResult;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * 网站控制器，处理博客相关的REST请求
  */
@@ -36,5 +41,27 @@ public class WebsiteController {
     public ResponseResult<String> updateBlog(@PathVariable String id, @RequestBody Blog blog) {
         String documentId = websiteService.updateBlog(id, blog);
         return ResponseResult.success("Blog updated with ID: " + documentId);
+    }
+
+    /**
+     * 获取博客文档，支持三种不同的获取方式：
+     * 1. 获取完整文档 (?pretty)
+     * 2. 获取指定字段 (?fields=field1,field2)
+     * 3. 仅获取source内容 (?sourceOnly=true)
+     */
+    @GetMapping("/{id}")
+    public ResponseResult<Map<String, Object>> getBlog(
+            @PathVariable String id,
+            @RequestParam(required = false) String fields,
+            @RequestParam(required = false, defaultValue = "false") boolean sourceOnly) {
+        Set<String> sourceFields = null;
+        if (fields != null && !fields.isEmpty()) {
+            sourceFields = new HashSet<>(Arrays.asList(fields.split(",")));
+        }
+        Map<String, Object> blog = websiteService.getBlog(id, sourceFields, sourceOnly);
+        if (blog == null) {
+            return ResponseResult.fail("Blog not found");
+        }
+        return ResponseResult.success(blog);
     }
 }
--- src/main/java/com/wenqi/springboot/elasticsearch/service/WebsiteService.java
+++ src/main/java/com/wenqi/springboot/elasticsearch/service/WebsiteService.java
@@ -3,14 +3,19 @@ package com.wenqi.springboot.elasticsearch.service;
 import com.alibaba.fastjson.JSON;
 import com.wenqi.springboot.elasticsearch.exception.BusinessException;
 import com.wenqi.springboot.elasticsearch.model.Blog;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.document.DocumentField;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * 网站服务类，处理博客相关操作
@@ -60,8 +65,57 @@ public class WebsiteService {
                 .source(JSON.toJSONString(blog), XContentType.JSON);
         try {
             return client.index(request, RequestOptions.DEFAULT).getId();
-        } catch (IOException e) {
+        } catch (Exception e) {
             throw new BusinessException("更新博客文档失败", e);
+        }
+    }
+
+    /**
+     * 获取博客文档，支持三种不同的获取方式：
+     * 1. 获取完整文档 (?pretty)
+     * 2. 获取指定字段 (?_source=field1,field2)
+     * 3. 仅获取source内容
+     *
+     * @param id 文档ID
+     * @param sourceFields 需要获取的字段，如果为null则获取全部字段
+     * @param sourceOnly 是否仅获取source内容
+     * @return 博客文档内容
+     */
+    public Map<String, Object> getBlog(String id, Set<String> sourceFields, boolean sourceOnly) {
+        GetRequest request = new GetRequest(INDEX, id);
+
+        // 如果指定了字段，则只获取指定字段
+        if (sourceFields != null && !sourceFields.isEmpty()) {
+            request.fetchSourceContext(new org.elasticsearch.search.fetch.subphase.FetchSourceContext(
+                    true,
+                    sourceFields.toArray(new String[0]),
+                    null
+            ));
+        }
+
+        try {
+            GetResponse response = client.get(request, RequestOptions.DEFAULT);
+
+            // 如果文档不存在，返回null
+            if (!response.isExists()) {
+                return null;
+            }
+
+            // 如果只需要source内容，直接返回source
+            if (sourceOnly) {
+                return response.getSourceAsMap();
+            }
+
+            // 返回完整文档，包括元数据
+            Map<String, Object> result = response.getSourceAsMap();
+            result.put("_index", response.getIndex());
+            result.put("_type", response.getType());
+            result.put("_id", response.getId());
+            result.put("_version", response.getVersion());
+            result.put("found", response.isExists());
+            return result;
+        } catch (Exception e) {
+            throw new BusinessException("获取博客文档失败", e);
         }
     }
 }
